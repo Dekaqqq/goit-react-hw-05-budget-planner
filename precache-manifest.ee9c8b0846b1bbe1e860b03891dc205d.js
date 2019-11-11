92ab5d5244d47560346cc6ff459e18a1e5b16270
dekaqqq <kdrushlyak@gmail.com>
2019-11-11 12:34:31 +0200

    Updates

---
+++ precache-manifest.ee9c8b0846b1bbe1e860b03891dc205d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b3e3986807ee3fba35508028bc68d6",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "f9d2db4157013ca68177",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.97b63c72.chunk.css"
+  },
+  {
+    "revision": "2ecdc66ad8d5efad5428",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.92192ff8.chunk.js"
+  },
+  {
+    "revision": "f9d2db4157013ca68177",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.dd2eea63.chunk.js"
+  },
+  {
+    "revision": "9bddad80ae55ecf11d43",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.a021a2fb.js"
+  }
+]);
